d6e400c5e76e3aee142047f3f69efa5a259bd589
pedrocaroli <pedrocaroli3001@gmail.com>
2026-01-19 13:34:43 -0300

    final airflow debuging

--- study_first_project/airflow/dags/orchestrator.py
+++ study_first_project/airflow/dags/orchestrator.py
@@ -1,13 +1,41 @@
-import sys
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime ,timedelta
-from docker.types import Mount as mount
 
-sys.path.append('/opt/airflow/api_request')
+def run_ingestion():
+    import sys
+    sys.path.append('/opt/airflow/api_request')
+    from insert_records import main
+    main()
 
-from insert_records import main
+def build_dbt_task():
+    from airflow.providers.docker.operators.docker import DockerOperator
+    from docker.types import Mount
+
+    return DockerOperator(
+        task_id='transform_data',
+        image='ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
+        mounts=[
+            Mount(
+                source='/home/caroli/lakehouse-workspace/study_first_project/dbt/my_project',
+                target='/usr/app',
+                type='bind'
+            ),
+            Mount(
+                source='/home/caroli/lakehouse-workspace/study_first_project/dbt/my_project/profiles.yml',
+                target='/root/.dbt/profiles.yml',
+                type='bind'
+            )
+        ],
+        auto_remove='success',
+        command='run --project-dir /usr/app --profiles-dir /root/.dbt',
+        docker_url='unix://var/run/docker.sock',
+        network_mode='study_first_project_my-network'
+    )
+
+
+
 
 default_args = {
     'description': 'A DAG to write dolar values into the database every day',
@@ -19,27 +47,15 @@ default_args = {
 dag = DAG(
     'dolar_api_orchestrator',
     default_args = default_args,
-    schedule = timedelta(days=1)
+    schedule = timedelta(minutes=15)
 )
 
 with dag:
     task1 = PythonOperator(
         task_id = 'data_ingestion_dolar_api',
-        python_callable = main
+        python_callable = run_ingestion
     )
 
-    task2 = DockerOperator(
-        task_id='transform_data',
-        image='ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
-        mounts=[
-            mount(source = '/home/caroli/lakehouse-workspace/study_first_project/dbt/my_project',
-            target = '/usr/app',
-            type = 'bind'),
-            mount(source = '/home/caroli/lakehouse-workspace/study_first_project/dbt/my_project/profiles.yml',target = '/root/.dbt/profiles.yml',type = 'bind')
-        ],
-        auto_remove='success',
-        command='run --project-dir /usr/app --profiles-dir /root/.dbt',
-        docker_url='unix://var/run/docker.sock',
-        network_mode='study_first_project_my-network')
+    task2 = build_dbt_task()
     
     task1 >> task2
